630516e138c0ef1ffddb8814d51706e215702c47
exect3r <exect3r@gmail.com>
2021-12-08 20:35:22 +0100

    added content and methods

--- front/components/LatestBlocks.vue
+++ front/components/LatestBlocks.vue
@@ -4,8 +4,19 @@
       <div class="card-title">Latest Blocks</div>
     </div>
     <div class="card-body">
-      <div v-if="latestBlocks.length > 0" class="entries">
-        <BlockEntry v-for="(block, i) of latestBlocks" :key="i" :data="block" class="entry" />
+      <div v-if="latestBlocks.length > 0" class="block-data">
+        <div v-for="(block, i) of latestBlocks" :key="i" class="entry">
+          <div class="data">
+            <div class="entry-hash">
+              <NuxtLink :to="{ path: '/block', query: {hash: block.hash} }">
+                {{ shortHash(block.hash) }}
+              </NuxtLink>
+            </div>
+            <div class="entry-date">
+              {{ elapsedTime(block.timestamp) }}
+            </div>
+          </div>
+        </div>
       </div>
       <div v-else>
         none
@@ -18,7 +29,26 @@
 export default {
   computed: {
     latestBlocks () {
-      return this.$store.state.latestBlocks
+      return [...this.$store.state.latestBlocks].reverse()
+    }
+  },
+  methods: {
+    shortHash (hash) {
+      return hash.slice(0, 4) + '—' + hash.slice(-4)
+    },
+    elapsedTime (timestamp) {
+      const time = Date.now() - timestamp
+      if (time < 1000) {
+        return 'just now'
+      } else if (time < 60000) {
+        return new Date(time).getSeconds() + 's ago'
+      } else if (time < 3600000) {
+        return new Date(time).getMinutes() + 'm ago'
+      } else if (time < 86400000) {
+        return new Date(time).getHours() + 'h ago'
+      } else {
+        return 'a long time ago'
+      }
     }
   }
 }
@@ -29,11 +59,22 @@ export default {
   width: 100%;
 }
 
-.entries {
+.block-data {
   overflow: hidden;
 }
 
-.entry:not(:last-child) {
-  border-bottom: black 1px solid;
+.block-data .entry::after {
+  content: '';
+  display: inline-block;
+  border-top: 1px solid var(--clr-bg-dec);
+  transition: border-color var(--clr-trans-spd) var(--clr-trans-tf);
+  transform: translateY(-.8em);
+  width: 100%;
+}
+
+.data {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 }
 </style>
--- front/components/LatestTransactions.vue
+++ front/components/LatestTransactions.vue
@@ -4,13 +4,19 @@
       <div class="card-title">Latest Transactions</div>
     </div>
     <div class="card-body">
-      <div v-if="latestTransactions.length > 0" class="entries">
-        <TransactionEntry
-          v-for="(trans, i) of latestTransactions"
-          :key="i"
-          :data="trans"
-          class="entry"
-        />
+      <div v-if="latestTransactions.length > 0" class="trans-data">
+        <div v-for="(trans, i) of latestTransactions" :key="i" class="entry">
+          <div class="data">
+            <div class="entry-hash">
+              <NuxtLink :to="{ path: '/transaction', query: {hash: trans.hash} }">
+                {{ shortHash(trans.id) }}
+              </NuxtLink>
+            </div>
+            <div class="entry-date">
+              {{ amount(trans) }} BeTC
+            </div>
+          </div>
+        </div>
       </div>
       <div v-else>
         none
@@ -23,7 +29,16 @@
 export default {
   computed: {
     latestTransactions () {
-      return this.$store.state.latestTransactions
+      return [...this.$store.state.latestTransactions].reverse()
+    }
+  },
+  methods: {
+    shortHash (hash) {
+      return hash.slice(0, 4) + '—' + hash.slice(-4)
+    },
+    amount (trans) {
+      return trans.data.outputs.filter(tx => !trans.data.inputs.find(txIn => txIn.address === tx.address))
+        .map(tx => tx.amount).reduce((a1, a2) => a1 + a2, 0)
     }
   }
 }
@@ -34,11 +49,22 @@ export default {
   width: 100%;
 }
 
-.entries {
+.trans-data {
   overflow: hidden;
 }
 
-.entry:not(:last-child) {
-  border-bottom: black 1px solid;
+.trans-data .entry::after {
+  content: '';
+  display: inline-block;
+  border-top: 1px solid var(--clr-bg-dec);
+  transition: border-color var(--clr-trans-spd) var(--clr-trans-tf);
+  transform: translateY(-.8em);
+  width: 100%;
+}
+
+.data {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 }
 </style>

990e493358741647a6a1d01a3165ba37343ef215
exect3r <exect3r@gmail.com>
2021-12-08 20:37:47 +0100

    removed unnecessary components

--- front/components/StatsSection.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <section id="stats" class="section">
-    <h2 class="section-title">Statistics</h2>
-
-    <div class="projects-grid">
-      put graphs here
-    </div>
-  </section>
-</template>
